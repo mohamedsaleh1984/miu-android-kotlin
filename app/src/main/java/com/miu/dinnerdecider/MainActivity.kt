f0c851c8a756aeff1b4aec967c924281faa346e0
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-02-19 16:31:35 -0500

    finished task b

--- app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.miu.dinnerdecider
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.miu.firstname.R
@@ -9,21 +8,30 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var food =arrayOf<String>("Hamburger","Pizza","Mexican", "American", "Chinese");
+    var food:ArrayList<String> =ArrayList<String>();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
 
         getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.gray_background));
+        food.add("Hamburger");
+        food.add("Pizza") ;
+        food.add("Mexican");
+        food.add( "American");
+        food.add( "Chinese");
     }
 
     fun onClickDecide(view: android.view.View){
         var randChoice= Random.nextInt(0, food.size);
         tvChoice.text = food[randChoice];
     }
-    fun onClickAdd(view: android.view.View) {
 
+    fun onClickAdd(view: android.view.View) {
+        if(editAddFood.text.length != 0){
+            food.add(editAddFood.text.toString());
+            editAddFood.getText().clear();
+        }
     }
 
 }
