b11379cf7ad7ee49768a4f402a5ee4d9a037b8ae
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-02-25 19:45:58 -0500

    Set Default Settings

--- app/src/main/java/com/miu/assignment/MainActivity.kt
+++ app/src/main/java/com/miu/assignment/MainActivity.kt
@@ -2,11 +2,32 @@ package com.miu.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.miu.firstname.R
+import android.widget.TableRow
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
     }
+
+    fun onClickAdd(view: android.view.View){
+        /*
+        val tableRow = TableRow(getApplicationContext())//this
+        //set new Table row Layout Paramters
+        val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        tableRow.setLayoutParams(layoutParams)
+        //Add values into row by calling addView()
+
+        var androidVersion = editTextAndroidVersion.getText();
+        var androidCode = editTextAndroidCode.getText();
+
+        tableRow.addView(androidVersion,0);
+        tableRow.addView(androidCode,1);
+        tableLayoutid.addView(tableRow) // id from Layout_file
+*/
+
+    }
+
 }
