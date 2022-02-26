74ac16dc59a5ec583822425aead079ab66fde2f2
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-02-26 06:33:15 -0500

    fix column width issue

--- app/src/main/java/com/miu/assignment/Walmart.kt
+++ app/src/main/java/com/miu/assignment/Walmart.kt
@@ -1,7 +1,7 @@
 package com.miu.assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class Walmart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
