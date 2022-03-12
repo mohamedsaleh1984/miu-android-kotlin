2fd0d41d628efdc770f3dd2c99fffc1c2ba663b6
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-03-12 11:01:32 -0500

    wip-task 2

--- app/src/main/java/com/miu/assigfour/ShoppingCategory.kt
+++ app/src/main/java/com/miu/assigfour/ShoppingCategory.kt
@@ -8,21 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_shopping_category.*
 
 class ShoppingCategory : AppCompatActivity() {
-    lateinit var userInfo: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_category)
-
-        val intent = intent
-        val userForTest = intent.getStringExtra("name")
-        //Toast.makeText(this,userForTest,Toast.LENGTH_SHORT).show()
-        val userFromMainActivity = intent.getSerializableExtra("user")
-        userInfo = userFromMainActivity as User;
-        //Toast.makeText(this,userInfo.firstName,Toast.LENGTH_SHORT).show()
-        tvUserEmailAddress.setText(userInfo.emailId)
     }
 
     fun onSelectCategory(view: View) {
-        Toast.makeText(this,"Selected Category is " + view.tag.toString(),Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this,"Selected Category is " + view.tag.toString(),Toast.LENGTH_SHORT).show()
     }
 }
