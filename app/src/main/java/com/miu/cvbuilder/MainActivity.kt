a0b15ff4d4a2150b8923f5435f261e8296223b09
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-04-03 12:33:56 -0400

    View Pager

--- app/src/main/java/com/miu/cvbuilder/MainActivity.kt
+++ app/src/main/java/com/miu/cvbuilder/MainActivity.kt
@@ -2,11 +2,19 @@ package com.miu.cvbuilder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // init views
+        viewPager = findViewById(R.id.fragmentHolder)
+       
     }
 }
