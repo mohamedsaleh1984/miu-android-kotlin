e7565f632676332271ba6dfecb7b9eecdd0dc4ce
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-04-06 20:20:48 -0400

    Handle PackageManger NPE

--- app/src/main/java/com/miu/cvbuilder/fragment/ContactFragment.kt
+++ app/src/main/java/com/miu/cvbuilder/fragment/ContactFragment.kt
@@ -5,13 +5,13 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.miu.cvbuilder.R
 import kotlinx.android.synthetic.main.fragment_contact.*
 
-class ContactFragment : Fragment( ) {
+class ContactFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,44 +23,60 @@ class ContactFragment : Fragment( ) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvPhone.setOnClickListener({CallNumber()})
-        img_phone.setOnClickListener({CallNumber()})
-        tvPhone2.setOnClickListener({CallNumber()})
+        tvPhone.setOnClickListener({ CallNumber() })
+        img_phone.setOnClickListener({ CallNumber() })
+        tvPhone2.setOnClickListener({ CallNumber() })
 
-        tv_email.setOnClickListener({SendEmail()})
-        email.setOnClickListener({SendEmail()})
-        emailImg.setOnClickListener({SendEmail()})
+        tv_email.setOnClickListener({ SendEmail() })
+        email.setOnClickListener({ SendEmail() })
+        emailImg.setOnClickListener({ SendEmail() })
 
-        tvlinkedIn.setOnClickListener({OpenLinkedIn()})
-        tvlinkedIn2.setOnClickListener({OpenLinkedIn()})
-        img_linkedIn.setOnClickListener({OpenLinkedIn()})
+        tvlinkedIn.setOnClickListener({ OpenLinkedIn() })
+        tvlinkedIn2.setOnClickListener({ OpenLinkedIn() })
+        img_linkedIn.setOnClickListener({ OpenLinkedIn() })
 
-        tvGit.setOnClickListener({OpenGitHub()})
-        tvGit2.setOnClickListener({OpenGitHub()})
-        gitImg.setOnClickListener({OpenGitHub()})
+        tvGit.setOnClickListener({ OpenGitHub() })
+        tvGit2.setOnClickListener({ OpenGitHub() })
+        gitImg.setOnClickListener({ OpenGitHub() })
     }
 
-    private fun OpenLinkedIn(){
+    private fun OpenLinkedIn() {
         val addressUri = Uri.parse("https://www.linkedin.com/in/mohamedsaleh1984/")
         val intent = Intent(Intent.ACTION_VIEW, addressUri)
-        getActivity()?.startActivity(intent)
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            getActivity()?.startActivity(intent)
+        } else {
+            Toast.makeText(activity, "Doesn't support this action", Toast.LENGTH_LONG).show()
+        }
     }
 
-    private fun SendEmail(){
+    private fun SendEmail() {
         val addressUri = Uri.parse("mailto:mohamedsaleh1984@hotmail.com")
         val intent = Intent(Intent.ACTION_SENDTO, addressUri)
-        getActivity()?.startActivity(intent)
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            getActivity()?.startActivity(intent)
+        } else {
+            Toast.makeText(activity, "Doesn't support this action", Toast.LENGTH_LONG).show()
+        }
     }
 
-    private fun CallNumber(){
-        val addressUri = Uri.parse("tel:+1292628798")
+    private fun CallNumber() {
+        val addressUri = Uri.parse("tel:+19292628798")
         val intent = Intent(Intent.ACTION_DIAL, addressUri)
-        getActivity()?.startActivity(intent)
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            getActivity()?.startActivity(intent)
+        } else {
+            Toast.makeText(activity, "Doesn't support this action", Toast.LENGTH_LONG).show()
+        }
     }
 
-    private fun OpenGitHub(){
+    private fun OpenGitHub() {
         val addressUri = Uri.parse("https://github.com/mohamedsaleh1984")
         val intent = Intent(Intent.ACTION_VIEW, addressUri)
-        getActivity()?.startActivity(intent)
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            getActivity()?.startActivity(intent)
+        } else {
+            Toast.makeText(activity, "Doesn't support this action", Toast.LENGTH_LONG).show()
+        }
     }
 }
